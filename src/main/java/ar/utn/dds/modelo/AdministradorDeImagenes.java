55f1c0d342353f06c199e402ca0007fe1623b602
Fedryus <30675879+Fedryus@users.noreply.github.com>
2019-07-27 21:22:17 -0300

    Creo al normalizador

---
+++ src/main/java/ar/utn/dds/modelo/AdministradorDeImagenes.java
@@ -0,0 +1,31 @@
+package ar.utn.dds.modelo;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class AdministradorDeImagenes {
+	
+	//Si la imagen no se encarga de normalizar, que haya una entidad que lo haga. 
+	//Crear esta entidad podria hacer más cosas con las imagenes si el sistema lo necesita (de ahi que no se llame solo 'normalizador')...
+	
+	public  void normalizarUnaImagen(String path,String destino, int width, int hight, String format) {
+    	File pathOrigen= new File(path);
+    	File pathDestino= new File(destino);
+    	try {
+    		BufferedImage original= ImageIO.read(pathOrigen);
+    		BufferedImage normalizada = new BufferedImage(width, hight, original.getType()); 
+    		
+    		Graphics2D g2= normalizada.createGraphics(); 
+    		g2.drawImage(original, 0, 0, width, hight, null);  
+    		g2.dispose();
+    		
+    		ImageIO.write(normalizada, format, pathDestino);  
+    	}
+    	catch(IOException ex) {
+    		ex.printStackTrace();}
+    	}
+}
