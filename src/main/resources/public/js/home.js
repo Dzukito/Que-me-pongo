9b032110bfb41867d7ff156500b7afa8c65951d4
Gabriela Galarza <gabriela-galarza@hotmail.com>
2019-11-02 20:54:13 -0300

    subo vista home usando bootstrap

--- src/main/resources/public/js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(".menu").on("click", function() {
-    $(".menu-options").toggle();
-  });
-  
-  $(".bla").on("click", function() {
-    window.location.href = "home.html"
-  });
-});
