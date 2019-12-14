c8d40dba43d0dd17482f33778e99313df4adf9a0
MartinAlejandroDiaz <diazmartin_95@yahoo.com.ar>
2019-12-14 04:52:51 -0300

    Feat/superponibles-test-percistencia

--- src/test/java/ar/utn/dds/persistencia/ContextTest.java
+++ src/test/java/ar/utn/dds/persistencia/ContextTest.java
@@ -13,8 +13,10 @@ import ar.utn.dds.db.EntityManagerHelper;
 import static org.junit.Assert.assertNotNull;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Assert;
 
@@ -304,7 +306,97 @@ public class ContextTest extends AbstractPersistenceTest implements WithGlobalEn
         EntityManagerHelper.commit();
     }
     @Test
-    public void recuperandoTipoDePrenda(){}
+    public void recuperandoTipoDePrenda(){
+        TipoPrenda tipo1 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 1").getSingleResult();
+        Assert.assertEquals("Remera cuello rendondo manga corta", tipo1.getTipo());
+
+        TipoPrenda tipo2 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 2").getSingleResult();
+        Assert.assertEquals("Remera cuello rendondo manga larga", tipo2.getTipo());
+
+        TipoPrenda tipo3 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 3").getSingleResult();
+        Assert.assertEquals("Remera escote V manga corta", tipo3.getTipo());
+
+        TipoPrenda tipo4 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 4").getSingleResult();
+        Assert.assertEquals("Remera escote V manga larga", tipo4.getTipo());
+
+        TipoPrenda tipo5 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 5").getSingleResult();
+        Assert.assertEquals("Sueter", tipo5.getTipo());
+
+        TipoPrenda tipo6 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 6").getSingleResult();
+        Assert.assertEquals("Campera", tipo6.getTipo());
+
+        TipoPrenda tipo7 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 7").getSingleResult();
+        Assert.assertEquals("Pantalon Largo", tipo7.getTipo());
+
+        TipoPrenda tipo8 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 8").getSingleResult();
+        Assert.assertEquals("Pantalon Corto", tipo8.getTipo());
+
+        TipoPrenda tipo9 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 9").getSingleResult();
+        Assert.assertEquals("Bermuda", tipo9.getTipo());
+        TipoPrenda tipo10 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 10").getSingleResult();
+        Assert.assertEquals("Pollera", tipo10.getTipo());
+        TipoPrenda tipo11 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 11").getSingleResult();
+        Assert.assertEquals("Calza", tipo11.getTipo());
+        TipoPrenda tipo12 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 12").getSingleResult();
+        Assert.assertEquals("Buzo", tipo12.getTipo());
+        TipoPrenda tipo13 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 13").getSingleResult();
+        Assert.assertEquals("Musculoza", tipo13.getTipo());
+        TipoPrenda tipo14 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 14").getSingleResult();
+        Assert.assertEquals("Zapatillas", tipo14.getTipo());
+        TipoPrenda tipo15 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 15").getSingleResult();
+        Assert.assertEquals("Zapatos", tipo15.getTipo());
+        TipoPrenda tipo16 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 16").getSingleResult();
+        Assert.assertEquals("Sandalias", tipo16.getTipo());
+        TipoPrenda tipo17 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 17").getSingleResult();
+        Assert.assertEquals("Manos", tipo17.getTipo());
+        TipoPrenda tipo18 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 18").getSingleResult();
+        Assert.assertEquals("Bufanda", tipo18.getTipo());
+        TipoPrenda tipo19 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 19").getSingleResult();
+        Assert.assertEquals("Gorro", tipo19.getTipo());
+        TipoPrenda tipo20 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 20").getSingleResult();
+        Assert.assertEquals(Categoria.ACCESORIOS.getCategoria(), tipo1.getTipo());
+        }
+    @Test
+    public void persistirPrendasSuperponibles(){
+        TipoPrenda tipo1 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 1").getSingleResult();
+        TipoPrenda tipo2 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 2").getSingleResult();
+        TipoPrenda tipo3 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 3").getSingleResult();
+        TipoPrenda tipo4 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 4").getSingleResult();
+        TipoPrenda tipo5 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 5").getSingleResult();
+        TipoPrenda tipo6 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 6").getSingleResult();
+        TipoPrenda tipo7 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 7").getSingleResult();
+        TipoPrenda tipo8 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 8").getSingleResult();
+        TipoPrenda tipo9 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 9").getSingleResult();
+        TipoPrenda tipo10 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 10").getSingleResult();
+        TipoPrenda tipo11 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 11").getSingleResult();
+        TipoPrenda tipo12 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 12").getSingleResult();
+        TipoPrenda tipo13 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 13").getSingleResult();
+        TipoPrenda tipo14 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 14").getSingleResult();
+        TipoPrenda tipo15 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 15").getSingleResult();
+        TipoPrenda tipo16 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 16").getSingleResult();
+        TipoPrenda tipo17 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 17").getSingleResult();
+        TipoPrenda tipo18 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 18").getSingleResult();
+        TipoPrenda tipo19 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 19").getSingleResult();
+        TipoPrenda tipo20 = (TipoPrenda) EntityManagerHelper.createQuery("from tipoPrenda where id_tipoPrenda = 20").getSingleResult();
+        tipo1.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11,tipo12)));
+        tipo2.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11,tipo12)));
+        tipo3.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11,tipo12)));
+        tipo4.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11,tipo12)));
+        tipo5.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo7,tipo6)));
+        tipo6.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11)));
+        tipo7.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo5,tipo6,tipo13)));
+        tipo8.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo6,tipo13)));
+        tipo9.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo6,tipo13)));
+        tipo10.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo6,tipo13)));
+        tipo11.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo6,tipo13)));
+        tipo12.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo7,tipo8,tipo9,tipo10,tipo13)));
+        tipo13.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo6,tipo8,tipo10,tipo11,tipo12)));
+        tipo14.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo5,tipo6,tipo7,tipo8,tipo9,tipo10,tipo11,tipo12,tipo13,tipo14,tipo15,tipo16)));
+        tipo15.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo7,tipo10)));
+        tipo16.setSuperponibles((Set<TipoPrenda>)new ArrayList<TipoPrenda>(Arrays.asList(tipo7,tipo8,tipo10)));
+    }
+    @Test
+    public void recuperandoPrendasSuperpobibles(){}
     @Test
     public void persistirPrendas(){}
     @Test
